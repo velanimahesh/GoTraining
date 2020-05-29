4950852429c7c2432913a1900f85d164e3ef43a1
Mahesh Velani <mahesh.velani@gslab.com>
2020-05-29 11:30:06 +0530

    Added variable exercise

---
+++ variables.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+// Define and initialize global variable
+var y = 10
+
+// Define var with default value
+//int  = 0
+//float = 0.0
+//string = ""
+//bool = false
+//nil : same as a null
+var z int
+
+func main()  {
+
+	// Print global variable y
+	fmt.Println(y)
+
+	// Print global var z
+	fmt.Println(z)
+
+	// define and print local variables
+	// this called a short declaration operator
+	x := 20
+	fmt.Println(x)
+
+	// update the value of z and y
+	y = 99
+	z = 100
+    foo()
+}
+
+func foo()  {
+	// here y = 99
+	fmt.Println("Foo : y = ", y)
+	// here z = 100
+	fmt.Println("Foo : z = ", z)
+}
