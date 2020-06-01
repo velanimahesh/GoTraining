b96f5fbbd1cf61c5a63a69691f2a4fa57ebd5ac6
Mahesh Velani <mahesh.velani@gslab.com>
2020-06-01 09:56:05 +0530

    added tyoe conversion example

---
+++ ownType.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+var z int = 50
+
+// Create your own type as hotdog
+type hotdog int
+var a hotdog
+
+func main()  {
+	a = 20
+
+	// Print z and it's type
+	fmt.Println(z)
+	fmt.Printf("%T\n", z)
+
+	// Print z and it's type
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+
+	// You can't assign value with diff type
+	// z = a
+	// This statement  will throw exception because int and hotdog is diff type
+	// To resolve this error we need to do type conversion
+	z = int(a)
+	// after converting a to int it will allow to assign because type of z is int
+	fmt.Println(z)
+	fmt.Printf("%T\n", z)
+}
